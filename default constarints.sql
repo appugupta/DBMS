3b04e3e4fcfee7c0f7d3ff6eedb75f6530b41f3a
Apurva gupta <91661911+appugupta@users.noreply.github.com>
2022-04-27 16:01:03 +0530

    Create default constarints.sql

---
+++ default constarints.sql
@@ -0,0 +1,8 @@
+CREATE TABLE EMPLOYEE(
+id number(5) NOT NULL ,
+Lname varhcar2(255) NOT NULL ,
+Fname varhcar2(255) ,
+age number(3) ,
+gender varchar(1) DEFAULT 'M',
+city varchar(255) DEFAULT 'Delhi'
+);
